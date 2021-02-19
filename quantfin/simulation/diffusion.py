0dfe91152e3c2600d30017deaaeaacb6226412db
Gustavo Amarante <gusamarante@gmail.com>
2021-02-18 22:03:17 -0300

    starting the diffusion objects

---
+++ quantfin/simulation/diffusion.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# TODO Simulate a simple brownian motion
+# TODO Simulate a BM coming from a normal distribution (optimization) and see if it yields simlar results
+# TODO Random walk with drift
+# TODO Log-normal random walk (Geometric BM)
+# TODO Mean-reverting random walk (Ornstein-Uhlenbeck)
+# TODO Mean-reverting Square root process
+# TODO General diffusion process class that takes mu(x,t), sigma(x,t) and y=f(x,t)
+# TODO correlated brownian motions (product rule for Ito, https://en.wikipedia.org/wiki/Itô%27s_lemma#Product_rule_for_Itô_processes)
+
+
+class Diffusion(object):
+    pass
