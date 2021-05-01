b90f90694540c6989604574b84fc627f5f9b1414
Gustavo Amarante <gusamarante@gmail.com>
2021-04-30 21:49:50 -0300

    KNN Chart Modification

--- chart_routines/knn_classifier.py
+++ chart_routines/knn_classifier.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_moons
 from matplotlib.colors import ListedColormap
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import validation_curve
+
+show_charts = False
+save_path = '/Users/gustavoamarante/Dropbox/Aulas/QuantFin/figures/'
 
 k_test = 51
 h = 0.02  # step size in the mesh
@@ -17,11 +19,20 @@ X, y = make_moons(noise=0.3,
                   n_samples=200)
 
 # plot the data
-cmap_light = ListedColormap(['orange', 'cyan'])
-cmap_bold = ['darkorange', 'c']
+cmap_light = ListedColormap(['#FFBC00', '#5FC4FF'])
+cmap_bold = ['#FF570A', '#2F34AF']
 
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(cmap_bold), edgecolor="black")
-plt.show()
+plt.figure(figsize=(6, 6))
+ax = plt.gca()
+ax.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(cmap_bold), edgecolor="black", label=None)
+ax.set(xlabel='$X$', ylabel='$Y$')
+plt.tight_layout()
+plt.savefig(save_path + 'KNN - simulated data.pdf')
+
+if show_charts:
+    plt.show()
+
+plt.close()
 
 # Decision region for K=1
 clf = KNeighborsClassifier(n_neighbors=1)
@@ -34,20 +45,29 @@ Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 # Put the result into a color plot
-plt.figure(figsize=(8, 6))
-plt.contourf(xx, yy, Z, cmap=cmap_light, alpha=1)
+plt.figure(figsize=(6, 6))
+plt.contourf(xx, yy, Z, cmap=cmap_light, alpha=0.6)
 
 # Plot also the training points
-sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, alpha=1, edgecolor="black", palette=cmap_bold)
+sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, alpha=1, edgecolor="black", palette=cmap_bold, legend=None)
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
+ax = plt.gca()
+ax.set(xlabel='$X$', ylabel='$Y$',
+       title='Classification with $K=1$')
+plt.tight_layout()
+plt.savefig(save_path + 'KNN - classification k=1.pdf')
 
-plt.show()
+if show_charts:
+    plt.show()
+
+plt.close()
+
 
 # estimate for several values of K (Cross-validation)
-train_scores, valid_scores = validation_curve(estimator=KNeighborsClassifier(), X=X, y=y, cv=10,
+train_scores, valid_scores = validation_curve(estimator=KNeighborsClassifier(), X=X, y=y, cv=2,
                                               param_name='n_neighbors', param_range=range(1, k_test),
-                                              verbose=2)
+                                              verbose=1)
 
 train_score = train_scores.mean(axis=1)
 test_score = valid_scores.mean(axis=1)
@@ -56,11 +76,21 @@ df = pd.DataFrame(data={'Train Score': train_score,
                         'Test Score': test_score},
                   index=range(1, k_test))
 
-df.plot()
-plt.show()
-
 K = df['Test Score'].idxmax()
 print(K)
+
+df.plot()
+ax = plt.gca()
+ax.set(xlabel='$K$ Nearest Neighbours', ylabel='% Correct Classifications')
+ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
+plt.tight_layout()
+plt.savefig(save_path + 'KNN - Cross validation.pdf')
+
+if show_charts:
+    plt.show()
+
+plt.close()
+
 
 # Decision region for K=optimal
 clf = KNeighborsClassifier(n_neighbors=K)
@@ -73,12 +103,52 @@ Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 # Put the result into a color plot
-plt.figure(figsize=(8, 6))
-plt.contourf(xx, yy, Z, cmap=cmap_light, alpha=1)
+plt.figure(figsize=(6, 6))
+plt.contourf(xx, yy, Z, cmap=cmap_light, alpha=0.6)
 
 # Plot also the training points
-sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, alpha=1, edgecolor="black", palette=cmap_bold)
+sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, alpha=1, edgecolor="black", palette=cmap_bold, legend=None)
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
 
-plt.show()
+ax = plt.gca()
+ax.set(xlabel='$X$', ylabel='$Y$',
+       title=f'Classification with $K={K}$')
+plt.tight_layout()
+plt.savefig(save_path + 'KNN - classification k=optimal.pdf')
+
+if show_charts:
+    plt.show()
+
+plt.close()
+
+
+# Decision region for K=50
+clf = KNeighborsClassifier(n_neighbors=50)
+clf.fit(X, y)
+
+x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+# Put the result into a color plot
+plt.figure(figsize=(6, 6))
+plt.contourf(xx, yy, Z, cmap=cmap_light, alpha=0.6)
+
+# Plot also the training points
+sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, alpha=1, edgecolor="black", palette=cmap_bold, legend=None)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+
+ax = plt.gca()
+ax.set(xlabel='$X$', ylabel='$Y$',
+       title=f'Classification with $K=50$')
+plt.tight_layout()
+plt.savefig(save_path + 'KNN - classification k=50.pdf')
+
+if show_charts:
+    plt.show()
+
+plt.close()
