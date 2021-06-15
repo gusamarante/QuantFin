89e068bd1eae21396516e324fedc4deb7f8eb0c7
Gustavo Amarante <gusamarante@gmail.com>
2021-06-15 12:09:24 -0300

    Trackers for long NTNB

--- quantfin/data/trackers/tracker_tesouronacional.py
+++ quantfin/data/trackers/tracker_tesouronacional.py
@@ -15,65 +15,65 @@ all_trackers = pd.DataFrame()
 # ===============
 # ===== LFT =====
 # ===============
-# df = df_raw[df_raw['bond_type'] == 'LFT']
-# df_buy = df.pivot('reference_date', 'maturity', 'preco_compra').dropna(how='all')
-# df_sell = df.pivot('reference_date', 'maturity', 'preco_venda').dropna(how='all')
-#
-# # ----- curto -----
-# df_tracker = pd.DataFrame(columns=['Current', 'Ammount', 'Price', 'Notional'])
-# df_tracker.loc[df_buy.index[0], 'Current'] = df_buy.iloc[0].dropna().index.min()
-# df_tracker.loc[df_buy.index[0], 'Ammount'] = 1
-# df_tracker.loc[df_buy.index[0], 'Price'] = df_buy.iloc[0][df_buy.iloc[0].dropna().index.min()]
-# df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'Price'] * df_tracker.loc[
-#     df_buy.index[0], 'Ammount']
-#
-# date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
-#
-# for date, datem1 in tqdm(date_loop, 'LFT - Curta'):
-#
-#     current = df_buy.loc[date].dropna().index.min()
-#     if current == df_tracker.loc[datem1, 'Current']:
-#         df_tracker.loc[date, 'Current'] = df_tracker.loc[datem1, 'Current']
-#         df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount']
-#         df_tracker.loc[date, 'Price'] = df_sell.loc[date, current]
-#         df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
-#
-#     else:
-#         df_tracker.loc[date, 'Current'] = current
-#         df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Notional'] / df_buy.loc[date, current]
-#         df_tracker.loc[date, 'Price'] = df_buy.loc[date, current]
-#         df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
-#
-# aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('LFT Curta')
-# all_trackers = pd.concat([all_trackers, aux], axis=1)
-#
-# # ----- longo -----
-# df_tracker = pd.DataFrame(columns=['Current', 'Ammount', 'Price', 'Notional'])
-# df_tracker.loc[df_buy.index[0], 'Current'] = df_buy.iloc[0].dropna().index.max()
-# df_tracker.loc[df_buy.index[0], 'Ammount'] = 1
-# df_tracker.loc[df_buy.index[0], 'Price'] = df_buy.iloc[0][df_buy.iloc[0].dropna().index.max()]
-# df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'Price'] * df_tracker.loc[
-#     df_buy.index[0], 'Ammount']
-#
-# date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
-#
-# for date, datem1 in tqdm(date_loop, 'LFT - Longa'):
-#
-#     current = df_buy.loc[date].dropna().index.max()
-#     if current == df_tracker.loc[datem1, 'Current']:
-#         df_tracker.loc[date, 'Current'] = df_tracker.loc[datem1, 'Current']
-#         df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount']
-#         df_tracker.loc[date, 'Price'] = df_sell.loc[date, current]
-#         df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
-#
-#     else:
-#         df_tracker.loc[date, 'Current'] = current
-#         df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Notional'] / df_buy.loc[date, current]
-#         df_tracker.loc[date, 'Price'] = df_buy.loc[date, current]
-#         df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
-#
-# aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('LFT Longa')
-# all_trackers = pd.concat([all_trackers, aux], axis=1)
+df = df_raw[df_raw['bond_type'] == 'LFT']
+df_buy = df.pivot('reference_date', 'maturity', 'preco_compra').dropna(how='all')
+df_sell = df.pivot('reference_date', 'maturity', 'preco_venda').dropna(how='all')
+
+# ----- curto -----
+df_tracker = pd.DataFrame(columns=['Current', 'Ammount', 'Price', 'Notional'])
+df_tracker.loc[df_buy.index[0], 'Current'] = df_buy.iloc[0].dropna().index.min()
+df_tracker.loc[df_buy.index[0], 'Ammount'] = 1
+df_tracker.loc[df_buy.index[0], 'Price'] = df_buy.iloc[0][df_buy.iloc[0].dropna().index.min()]
+df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'Price'] * df_tracker.loc[
+    df_buy.index[0], 'Ammount']
+
+date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
+
+for date, datem1 in tqdm(date_loop, 'LFT - Curta'):
+
+    current = df_buy.loc[date].dropna().index.min()
+    if current == df_tracker.loc[datem1, 'Current']:
+        df_tracker.loc[date, 'Current'] = df_tracker.loc[datem1, 'Current']
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount']
+        df_tracker.loc[date, 'Price'] = df_sell.loc[date, current]
+        df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
+
+    else:
+        df_tracker.loc[date, 'Current'] = current
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Notional'] / df_buy.loc[date, current]
+        df_tracker.loc[date, 'Price'] = df_buy.loc[date, current]
+        df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
+
+aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('LFT Curta')
+all_trackers = pd.concat([all_trackers, aux], axis=1)
+
+# ----- longo -----
+df_tracker = pd.DataFrame(columns=['Current', 'Ammount', 'Price', 'Notional'])
+df_tracker.loc[df_buy.index[0], 'Current'] = df_buy.iloc[0].dropna().index.max()
+df_tracker.loc[df_buy.index[0], 'Ammount'] = 1
+df_tracker.loc[df_buy.index[0], 'Price'] = df_buy.iloc[0][df_buy.iloc[0].dropna().index.max()]
+df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'Price'] * df_tracker.loc[
+    df_buy.index[0], 'Ammount']
+
+date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
+
+for date, datem1 in tqdm(date_loop, 'LFT - Longa'):
+
+    current = df_buy.loc[date].dropna().index.max()
+    if current == df_tracker.loc[datem1, 'Current']:
+        df_tracker.loc[date, 'Current'] = df_tracker.loc[datem1, 'Current']
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount']
+        df_tracker.loc[date, 'Price'] = df_sell.loc[date, current]
+        df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
+
+    else:
+        df_tracker.loc[date, 'Current'] = current
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Notional'] / df_buy.loc[date, current]
+        df_tracker.loc[date, 'Price'] = df_buy.loc[date, current]
+        df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
+
+aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('LFT Longa')
+all_trackers = pd.concat([all_trackers, aux], axis=1)
 
 
 # =================
@@ -104,7 +104,7 @@ df_buy = df.pivot_table(index='reference_date', columns='maturity', values='prec
 df_sell = df.pivot_table(index='reference_date', columns='maturity', values='preco_venda', aggfunc='mean').dropna(how='all')
 
 
-# ----- curto -----
+# ----- curta -----
 df_tracker = pd.DataFrame(columns=['Current', 'Coupon', 'Ammount', 'Price', 'Notional'])
 
 # Choose the first bond
@@ -122,8 +122,6 @@ df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'P
 date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
 
 for date, datem1 in tqdm(date_loop, 'NTNB - Curta'):
-    if date == '2019-02-14 00:00:00':
-        a = 1
 
     # check if there are coupon payments for this date
     if date in coupon_dates:
@@ -150,8 +148,53 @@ for date, datem1 in tqdm(date_loop, 'NTNB - Curta'):
 aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('NTNB Curta')
 all_trackers = pd.concat([all_trackers, aux], axis=1)
 
+
+# ----- longa -----
+df_tracker = pd.DataFrame(columns=['Current', 'Coupon', 'Ammount', 'Price', 'Notional'])
+
+# Choose the first bond
+current = df_buy.iloc[0].dropna().index.max()  # selects the current bond on the initial date
+coupon_dates = get_coupon_dates(df_buy.index[0], current)
+
+df_tracker.loc[df_buy.index[0], 'Current'] = current
+df_tracker.loc[df_buy.index[0], 'Coupon'] = 0
+df_tracker.loc[df_buy.index[0], 'Ammount'] = 1  # Number of bonds to buy
+df_tracker.loc[df_buy.index[0], 'Price'] = df_buy.iloc[0][current]
+df_tracker.loc[df_buy.index[0], 'Notional'] = df_tracker.loc[df_buy.index[0], 'Price'] * df_tracker.loc[
+    df_buy.index[0], 'Ammount']
+
+date_loop = zip(df_buy.index[1:], df_buy.index[:-1])
+
+for date, datem1 in tqdm(date_loop, 'NTNB - Longa'):
+
+    # check if there are coupon payments for this date
+    if date in coupon_dates:
+        df_tracker.loc[date, 'Coupon'] = df_vna.loc[pd.to_datetime(date), 'VNA'] * (np.sqrt(1.06) - 1)
+    else:
+        df_tracker.loc[date, 'Coupon'] = 0
+
+    # Chooses the new bond to invest
+    current = df_buy.loc[date].dropna().index.max()
+    if current == df_tracker.loc[datem1, 'Current']:
+        df_tracker.loc[date, 'Current'] = df_tracker.loc[datem1, 'Current']
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount'] * (1 + df_tracker.loc[date, 'Coupon']/df_buy.loc[date, current])
+
+    else:
+        df_tracker.loc[date, 'Current'] = current
+        df_tracker.loc[date, 'Ammount'] = df_tracker.loc[datem1, 'Ammount'] * ((df_sell.loc[date, current] + df_tracker.loc[date, 'Coupon']) / df_buy.loc[date, current])
+
+    df_tracker.loc[date, 'Price'] = df_sell.loc[date, current]
+    df_tracker.loc[date, 'Notional'] = df_tracker.loc[date, 'Price'] * df_tracker.loc[date, 'Ammount']
+
+aux = (100 * df_tracker['Notional'] / df_tracker['Notional'].iloc[0]).rename('NTNB Longa')
+all_trackers = pd.concat([all_trackers, aux], axis=1)
+
 # Chart (temporary)
 all_trackers.index = pd.to_datetime(list(all_trackers.index))
-all_trackers.to_clipboard()
-all_trackers.plot()
+returns = all_trackers.pct_change(252)
+std = all_trackers.pct_change(1).ewm(halflife=252).std() * np.sqrt(252)
+sharpe = returns / std
+
+sharpe.plot()
+plt.tight_layout()
 plt.show()
