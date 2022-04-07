c921ec6d0f68b533890af2b417ee22b84814d121
gustavoamarante <gusamarante@gmail.com>
2022-04-07 09:14:48 -0300

    Added wrapper for the SGS data API from the BCB

--- quantfin/data/__init__.py
+++ quantfin/data/__init__.py
@@ -1,3 +1,4 @@
 from quantfin.data.database_utilities import grab_connection, tracker_uploader, tracker_feeder
+from quantfin.data.bcbsgs import SGS
 
-__all__ = ['grab_connection', 'tracker_uploader', 'tracker_feeder']
+__all__ = ['grab_connection', 'tracker_uploader', 'tracker_feeder', 'SGS']
