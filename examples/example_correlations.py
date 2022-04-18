e18b54629f1d5e25c845e2a40cbf727a1b794ca7
gamarante <gusamarante@gmail.com>
2022-04-18 13:12:34 -0200

    Ledoit-Wolfe Covariance, target shrinkage covariance and detoning correlations matrix

--- examples/example_correlations.py
+++ examples/example_correlations.py
@@ -1,4 +1,5 @@
-from quantfin.statistics import empirical_covariance, shrink_cov, marchenko_pastur, cov2corr, targeted_shirinkage
+from quantfin.statistics import empirical_covariance, shrink_cov, marchenko_pastur, cov2corr, targeted_shirinkage, \
+    ledoitwolf_cov, detone_corr
 from quantfin.data import tracker_feeder, SGS
 from quantfin.finmath import compute_eri
 import matplotlib.pyplot as plt
@@ -20,33 +21,39 @@ df_cdi = df_cdi / 100
 
 # Compute ERIs
 df_eri = compute_eri(total_return_index=df, funding_return=df_cdi['CDI'])
-df_returns = df_eri.pct_change(1)
+df_returns = df_eri.pct_change(1).dropna()
 
 # Correlation
 emp_cov = empirical_covariance(df_returns)
 emp_corr, _ = cov2corr(emp_cov)
-print(emp_corr, '\n')
+# print(emp_corr, '\n')
 
 # Shirinkage
 shrunk_cov = shrink_cov(df_returns, alpha=0.5)
 shrunk_corr, _ = cov2corr(shrunk_cov)
-print(shrunk_corr, '\n')
+# print(shrunk_corr, '\n')
 
 # Marchenko-Pastur
 mp_cov, _, _ = marchenko_pastur(df_returns)
 mp_corr, _ = cov2corr(mp_cov)
-print(mp_corr, '\n')
+# print(mp_corr, '\n')
 
 # Targeted Shrinkage
-ts_cov, _, _ = targeted_shirinkage(df_returns, ts_alpha=1)  # TODO deveria dar o MP?
+ts_cov, _, _ = targeted_shirinkage(df_returns, ts_alpha=0.5)  # TODO deveria dar o MP?
 ts_corr, _ = cov2corr(ts_cov)
-print(ts_corr, '\n')
+# print(ts_corr, '\n')
 
 # Ledoit-Wolfe
-
+lw_cov = ledoitwolf_cov(df_returns)
+lw_corr, _ = cov2corr(lw_cov)
+# print(lw_corr, '\n')
 
 # Detoning
-
+detone_corr = detone_corr(emp_corr, n=1)
+print(detone_corr, '\n')
 
 # Plot
-print(ts_corr - mp_corr)
+dif = detone_corr - emp_corr
+print(dif)
+
+print(np.abs(dif).max().max())
