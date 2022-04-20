cdd293d6207f0fa310a7c7c802d5f194557ff626
gamarante <gusamarante@gmail.com>
2022-04-20 16:11:13 -0200

    Asset selection routine

--- chartbook/available_assets.py
+++ chartbook/available_assets.py
@@ -1,9 +1,10 @@
-from quantfin.charts import timeseries, df2pdf, df2heatmap
 from quantfin.data import tracker_feeder, SGS
 from quantfin.portfolio import Performance
 from quantfin.finmath import compute_eri
+from quantfin.charts import timeseries
 import matplotlib.pyplot as plt
 from pathlib2 import Path
+from tqdm import tqdm
 import pandas as pd
 
 # TODO add rolling measures to chartbook
@@ -33,11 +34,34 @@ df_eri = df_eri[df_eri.index >= '2010-01-01']
 
 # Performance Data
 perf_t = Performance(df_eri)
-perf_t.table.to_clipboard()
-print('Copied performance table')
+df_perf = perf_t.table.T
+df_perf = df_perf[df_perf['Sharpe'] >= 0]  # Filter positive sharpe
+df_score = (df_perf - df_perf.min(axis=0)) / (df_perf.max(axis=0) - df_perf.min(axis=0))  # normalize indicators
+df_score['Kurt'] = 1 - df_score['Kurt']  # Invert
+df_score['Start Date'] = 1 - df_score['Start Date']  # Invert
+df_score = df_score[df_score['Start Date'] >= 0.1]  # Exclude series that are too short
+
+try:
+    df_score['Score'] = (1 * df_score['Sharpe']
+                         + 0.5 * df_score['Skew']
+                         + 0.2 * df_score['Kurt']
+                         + 0.5 * df_score['Max DD']) \
+                        / (1 + 0.5 + 0.2 + 0.5)
+except KeyError:
+    df_score['Score'] = (1 * df_score['Sharpe']
+                         + 0.5 * df_score['Skew']
+                         + 0.2 * df_score['Kurt']) \
+                        / (1 + 0.5 + 0.2)
+
+df_perf['Score'] = df_score['Score']
+df_perf = df_perf.dropna()
+
+writer = pd.ExcelWriter(r'C:\Users\gamarante\Dropbox\Personal Portfolio\Available Assets.xlsx')
+df_perf.to_excel(writer, 'Filtered')
+writer.save()
 
 # Charts
-for asset in df_eri.columns:
+for asset in tqdm(df_eri.columns, 'Generating Charts'):
 
     df_plot = df_tri[asset].dropna()
     timeseries(df_plot, title=f'{asset} - Total Return Index',
