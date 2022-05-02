5f3a2ef896abb1ddc5dc34d4c7c84dde93a1168b
gustavoamarante <gusamarante@gmail.com>
2022-05-01 21:02:16 -0300

    easier way to run the scrapper from XP

--- quantfin/data/scrape/cota_xp.py
+++ quantfin/data/scrape/cota_xp.py
@@ -22,82 +22,83 @@ titles = ['Patrimônio inicial (R$)',
           'Rentabilidade (%)',
           'Rentabilidade (% CDI)']
 
-with open('/Users/gustavoamarante/Dropbox/Personal Portfolio/fonte da XP.txt', encoding='utf-8') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
-
 # Empty Dataframes
 df_year = pd.DataFrame(columns=titles)
 df_month = pd.DataFrame(columns=titles)
 df_day = pd.DataFrame(columns=titles)
 
-years2loop = soup.find_all('soma-table-row', level='year')
+for yy in [2017, 2018, 2019, 2020, 2021, 2022]:
+    with open(DROPBOX.joinpath(f'fonte da XP {yy}.txt'), encoding='utf-8') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
 
-for soupy in tqdm(years2loop, 'Years'):
-    try:
-        current_year = int(soupy.find_all('soma-table-cell')[0].string)
-    except ValueError:
-        continue
+    years2loop = soup.find_all('soma-table-row', level='year')
 
-    cells = soupy.find_all('soma-table-cell')
-
-    # Save Yearly Values
-    df_year.loc[current_year, 'Patrimônio inicial (R$)'] = float(cells[1].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-    df_year.loc[current_year, 'Movimentações (R$)'] = float(cells[2].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-    df_year.loc[current_year, 'IR pago (R$)'] = float(cells[3].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-    df_year.loc[current_year, 'Patrimônio final (R$)'] = float(cells[4].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-    df_year.loc[current_year, 'Rendimento (R$)'] = float(cells[5].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-    df_year.loc[current_year, 'Rentabilidade (%)'] = float(cells[6].string.replace('%', '').replace('.', '').replace(',', '.')) / 100
-    df_year.loc[current_year, 'Rentabilidade (% CDI)'] = float(cells[7].string.replace('%', '').replace('.', '').replace(',', '.')) / 100
-
-    # Save monthly values
-
-    months2loop = soupy.find_all('soma-table-row', level='month')
-
-    for soupm in months2loop:
-        month_elements = soupm.find_all('soma-table-cell')
-
-        find_month = list(month_elements[0].strings)
-        find_month = find_month[-1].replace('\t', '').replace('\n', '')
-
-        idx_date = pd.to_datetime(str(current_year) + '-' + str(months[find_month]) + '-1') + MonthEnd(0)
-
-        df_month.loc[idx_date, 'Patrimônio inicial (R$)'] = float(month_elements[1].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-        df_month.loc[idx_date, 'Movimentações (R$)'] = float(month_elements[2].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-        df_month.loc[idx_date, 'IR pago (R$)'] = float(month_elements[3].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-        df_month.loc[idx_date, 'Patrimônio final (R$)'] = float(month_elements[4].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-        df_month.loc[idx_date, 'Rendimento (R$)'] = float(month_elements[5].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-
+    for soupy in tqdm(years2loop, str(yy)):
         try:
-            df_month.loc[idx_date, 'Rentabilidade (%)'] = float(month_elements[6].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
+            current_year = int(soupy.find_all('soma-table-cell')[0].string)
         except ValueError:
-            df_month.loc[idx_date, 'Rentabilidade (%)'] = np.nan
+            continue
 
-        try:
-            df_month.loc[idx_date, 'Rentabilidade (% CDI)'] = float(month_elements[7].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
-        except ValueError:
-            df_month.loc[idx_date, 'Rentabilidade (% CDI)'] = np.nan
+        cells = soupy.find_all('soma-table-cell')
 
-        # Save daily values
-        number_days = int(len(month_elements[8:]) / 8)
-        for soupd in range(1, number_days):
-            day = int(month_elements[soupd * 8].string.replace('\t', '').replace('\n', ''))
-            idx_day = pd.to_datetime(date(current_year, months[find_month], day))
+        # Save Yearly Values
+        df_year.loc[current_year, 'Patrimônio inicial (R$)'] = float(cells[1].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        df_year.loc[current_year, 'Movimentações (R$)'] = float(cells[2].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        df_year.loc[current_year, 'IR pago (R$)'] = float(cells[3].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        df_year.loc[current_year, 'Patrimônio final (R$)'] = float(cells[4].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        df_year.loc[current_year, 'Rendimento (R$)'] = float(cells[5].string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        df_year.loc[current_year, 'Rentabilidade (%)'] = float(cells[6].string.replace('%', '').replace('.', '').replace(',', '.')) / 100
+        df_year.loc[current_year, 'Rentabilidade (% CDI)'] = float(cells[7].string.replace('%', '').replace('.', '').replace(',', '.')) / 100
 
-            df_day.loc[idx_day, 'Patrimônio inicial (R$)'] = float(month_elements[soupd * 8 + 1].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-            df_day.loc[idx_day, 'Movimentações (R$)'] = float(month_elements[soupd * 8 + 2].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-            df_day.loc[idx_day, 'IR pago (R$)'] = float(month_elements[soupd * 8 + 3].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-            df_day.loc[idx_day, 'Patrimônio final (R$)'] = float(month_elements[soupd * 8 + 4].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
-            df_day.loc[idx_day, 'Rendimento (R$)'] = float(month_elements[soupd * 8 + 5].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+        # Save monthly values
+
+        months2loop = soupy.find_all('soma-table-row', level='month')
+
+        for soupm in months2loop:
+            month_elements = soupm.find_all('soma-table-cell')
+
+            find_month = list(month_elements[0].strings)
+            find_month = find_month[-1].replace('\t', '').replace('\n', '')
+
+            idx_date = pd.to_datetime(str(current_year) + '-' + str(months[find_month]) + '-1') + MonthEnd(0)
+
+            df_month.loc[idx_date, 'Patrimônio inicial (R$)'] = float(month_elements[1].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+            df_month.loc[idx_date, 'Movimentações (R$)'] = float(month_elements[2].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+            df_month.loc[idx_date, 'IR pago (R$)'] = float(month_elements[3].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+            df_month.loc[idx_date, 'Patrimônio final (R$)'] = float(month_elements[4].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+            df_month.loc[idx_date, 'Rendimento (R$)'] = float(month_elements[5].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
 
             try:
-                df_day.loc[idx_day, 'Rentabilidade (%)'] = float(month_elements[soupd * 8 + 6].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
+                df_month.loc[idx_date, 'Rentabilidade (%)'] = float(month_elements[6].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
             except ValueError:
-                df_day.loc[idx_day, 'Rentabilidade (%)'] = np.nan
+                df_month.loc[idx_date, 'Rentabilidade (%)'] = np.nan
 
             try:
-                df_day.loc[idx_day, 'Rentabilidade (% CDI)'] = float(month_elements[soupd * 8 + 7].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
+                df_month.loc[idx_date, 'Rentabilidade (% CDI)'] = float(month_elements[7].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
             except ValueError:
-                df_day.loc[idx_day, 'Rentabilidade (% CDI)'] = np.nan
+                df_month.loc[idx_date, 'Rentabilidade (% CDI)'] = np.nan
+
+            # Save daily values
+            number_days = int(len(month_elements[8:]) / 8)
+            for soupd in range(1, number_days):
+                day = int(month_elements[soupd * 8].string.replace('\t', '').replace('\n', ''))
+                idx_day = pd.to_datetime(date(current_year, months[find_month], day))
+
+                df_day.loc[idx_day, 'Patrimônio inicial (R$)'] = float(month_elements[soupd * 8 + 1].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+                df_day.loc[idx_day, 'Movimentações (R$)'] = float(month_elements[soupd * 8 + 2].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+                df_day.loc[idx_day, 'IR pago (R$)'] = float(month_elements[soupd * 8 + 3].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+                df_day.loc[idx_day, 'Patrimônio final (R$)'] = float(month_elements[soupd * 8 + 4].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+                df_day.loc[idx_day, 'Rendimento (R$)'] = float(month_elements[soupd * 8 + 5].find('span').string.replace(u'\xa0', '').replace('R$', '').replace('.', '').replace(',', '.'))
+
+                try:
+                    df_day.loc[idx_day, 'Rentabilidade (%)'] = float(month_elements[soupd * 8 + 6].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
+                except ValueError:
+                    df_day.loc[idx_day, 'Rentabilidade (%)'] = np.nan
+
+                try:
+                    df_day.loc[idx_day, 'Rentabilidade (% CDI)'] = float(month_elements[soupd * 8 + 7].string.replace('\t', '').replace('\n', '').replace('%', '').replace('.', '').replace(',', '.')) / 100
+                except ValueError:
+                    df_day.loc[idx_day, 'Rentabilidade (% CDI)'] = np.nan
 
 df_year = df_year.sort_index()
 df_month = df_month.sort_index()
@@ -111,7 +112,7 @@ tracker = 100 * tracker / tracker.iloc[0]
 df_day['tracker'] = tracker
 
 # Save to Excel
-writer = pd.ExcelWriter(DROPBOX.joinpath('/trackers/XP.xlsx'))
+writer = pd.ExcelWriter(DROPBOX.joinpath('trackers/XP.xlsx'))
 df_year.to_excel(writer, 'year')
 df_month.to_excel(writer, 'month')
 df_day.to_excel(writer, 'day')
