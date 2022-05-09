fcf4dc92bdf3f9ada6cefab03eb7494a320aa8eb
gamarante <gusamarante@gmail.com>
2022-05-09 16:04:18 -0200

    minor changes

--- examples/daa.py
+++ examples/daa.py
@@ -30,13 +30,14 @@ df_eri = compute_eri(df_tri, df_cdi)
 
 # Get HMM
 hmm = GaussianHMM(returns=df_eri.resample('M').last().pct_change().dropna())
-hmm.fit(n_states=3, fit_iter=100)
+hmm.fit(n_states=4, fit_iter=100)
 
 # DAA - Testing with pandas input
 Lambda0 = (15 / 10000) * np.eye(df_eri.shape[1])
 Lambda1 = (20 / 10000) * np.eye(df_eri.shape[1])
 Lambda2 = (50 / 10000) * np.eye(df_eri.shape[1])
-Lambda = np.array([Lambda0, Lambda1, Lambda2])
+Lambda3 = (50 / 10000) * np.eye(df_eri.shape[1])
+Lambda = np.array([Lambda0, Lambda1, Lambda2, Lambda3])
 
 allocations = np.array([100, 100, 100, 100])
 
@@ -47,14 +48,13 @@ daa = DAACosts(means=hmm.means,
                costs=Lambda,
                transition_matrix=hmm.trans_mat,
                current_allocation=allocations,
-               risk_aversion=1,
+               risk_aversion=5e-8,
                discount_factor=0.99,
                include_returns=True,
                normalize=True)
 
 print('allocations', daa.allocations)
 print('aims', daa.aim_portfolios)
-print('markowitz', daa.markowitz_portfolios)
 
 ldaa = DAALinearCosts(means=hmm.means,
                       covars=hmm.covars,
@@ -67,6 +67,5 @@ ldaa = DAALinearCosts(means=hmm.means,
                       normalize=True)
 
 print('linear allocations', ldaa.allocations)
-print('linear markowitz', ldaa.markowitz_portfolios)
 
 # TODO ternary plot com a evolução do portfolio
