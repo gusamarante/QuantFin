d895ed072b31b36a0c31af7b0404434e52dbc0c1
gamarante <gusamarante@gmail.com>
2022-05-20 10:13:44 -0200

    minor changes

--- quantfin/data/total_return_fetch.py
+++ quantfin/data/total_return_fetch.py
@@ -1,16 +1,13 @@
 """
 This routine grabs the trackers built and adds them to my database to easily handle them later.
 """
-import getpass
 import pandas as pd
-from pathlib import Path
 import matplotlib.pyplot as plt
+from quantfin.data import DROPBOX
 from quantfin.data import tracker_uploader
 
 df = pd.DataFrame()
-username = getpass.getuser()
-# file_path = Path(r"C:\Users\gamarante\Dropbox\Personal Portfolio\trackers")  # BW
-file_path = Path(f'/Users/{username}/Dropbox/Personal Portfolio/trackers')  # Mac
+file_path = DROPBOX.joinpath('trackers')
 
 # ============================
 # ===== Tesouro Nacional =====
