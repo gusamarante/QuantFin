87830d93ed2033a13704ec6055c5cd0f56a111e4
gustavoamarante <gusamarante@gmail.com>
2022-05-20 17:46:46 -0300

    Minor changes

--- examples/quantfin_monitoria2.py
+++ examples/quantfin_monitoria2.py
@@ -1,1 +1,21 @@
+import pandas as pd
+
+# Change the address according to your computer
+file = r'/Users/gustavoamarante/Dropbox/Aulas/Insper - Financas Quantitativas/2022/Monitoria 2/Dados Monitoria 2.xlsx'
+df = pd.read_excel(file, sheet_name='Sheet1', index_col=0)
+
+# Ex ante vol
+
+# Predictability - Equation 2 / Fig 1
+
+# Predictability - Equation 3 / Fig 1
+
+# Timeseries Momentum Factor - Eq 5 and 5.1 / Fig 2
+
+# Passive Long VS TSM Factor - Fig 3
+
+# TSMoM smile - Fig 4
+
+# EXTRA - Cross-Section Momentum Factor
+
 
