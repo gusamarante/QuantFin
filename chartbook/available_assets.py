229a2a146f71baa6687bc94b785b7bdd87e6590e
gustavoamarante <gusamarante@gmail.com>
2022-05-20 17:26:36 -0300

    Minor changes

--- chartbook/available_assets.py
+++ chartbook/available_assets.py
@@ -27,8 +27,8 @@ df_cdi = sgs.fetch({12: 'CDI'})
 df_cdi = df_cdi['CDI'] / 100
 
 # Compute ERI
+df_tri = df_tri[df_tri.index >= '2006-01-01']
 df_eri = compute_eri(df_tri, df_cdi)
-df_eri = df_eri[df_eri.index >= '2010-01-01']
 
 # Performance Data
 perf_t = Performance(df_eri)
