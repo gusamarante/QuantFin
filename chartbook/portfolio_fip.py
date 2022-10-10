ee97f7e1e58d232eb0d02f58ae9b44b60d93d0f2
gusamarante <Jmba@160959>
2022-10-09 23:37:35 -0300

    Finished the FIPP Portfolio

--- chartbook/portfolio_fip.py
+++ chartbook/portfolio_fip.py
@@ -32,15 +32,7 @@ bt_ew = EqualWeights(df_eri)
 bt_hrp = BacktestHRP(eri=df_eri, cov=df_cov, rebalance_dates=rebalance_dates,
                      method='complete', metric='braycurtis')
 
-# hrp = HRP(cov=df_cov.xs(df_cov.index.get_level_values(0).max()),
-#           method='complete', metric='braycurtis')
-# hrp.plot_dendrogram(show_chart=show_charts,
-#                     save_path=DROPBOX.joinpath(r'charts/HRP - Dendrogram.pdf'))
-# hrp.plot_corr_matrix(save_path=DROPBOX.joinpath(r'charts/HRP - Correlation matrix.pdf'),
-#                      show_chart=show_charts)
-
 # Equal Risk Contribution
-
 bt_erc = BacktestERC(eri=df_eri, cov=df_cov, rebalance_dates=rebalance_dates, name=f'ERC')
 
 # ===== COMPARE PERFORMANCE =====
@@ -50,7 +42,20 @@ df_compare = pd.concat([bt_ew.return_index.rename('EW'),
                        axis=1)
 
 perf = Performance(df_compare)
-print(perf.table)
+chosen_method = perf.table.loc['Sharpe'].astype(float).idxmax()
+print('Chosen method for FIP is', chosen_method)
 
-df_compare.plot()
-plt.show()
+if chosen_method == 'EW':
+    weights = bt_ew.weights.iloc[-1].rename('EW Weights')
+    tracker = bt_ew.return_index
+elif chosen_method == 'HRP':
+    weights = bt_hrp.weights.iloc[-1].rename('HRP Weights')
+    tracker = bt_hrp.return_index
+elif chosen_method == 'ERC':
+    weights = bt_erc.weights.iloc[-1].rename('ERC Weights')
+    tracker = bt_hrp.return_index
+
+writer = pd.ExcelWriter(DROPBOX.joinpath(f'Pillar FIP.xlsx'))
+weights.to_excel(writer, 'FIP Weights')
+tracker.to_excel(writer, 'FIP Trackrers')
+writer.save()
