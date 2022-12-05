40f717af6a5932ed74ea32fbacca91bf6601dddf
gusamarante <Jmba@160959>
2022-12-05 18:02:18 -0300

    progress on the real rates portfolio

--- chartbook/portfolio_real_rates.py
+++ chartbook/portfolio_real_rates.py
@@ -1,5 +1,8 @@
+from scipy.optimize import minimize, Bounds, NonlinearConstraint, LinearConstraint
 from quantfin.data import tracker_feeder, DROPBOX, SGS
 from quantfin.portfolio import Performance
+from quantfin.statistics import corr2cov
+from quantfin.finmath import compute_eri
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pandas as pd
@@ -7,6 +10,8 @@ import numpy as np
 
 last_year = 2022
 start_date = '2007-01-01'
+min_vol = 0.06
+rebalance_window = 3  # in months
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 50)
@@ -19,6 +24,14 @@ pd.set_option('display.width', 250)
 df_tri = tracker_feeder()
 df_tri = df_tri[['NTNB 0.5y', 'NTNB 1y', 'NTNB 1.5y', 'NTNB 2y', 'NTNB 3y', 'NTNB 4y',
                  'NTNB 5y', 'NTNB 6y', 'NTNB 7y', 'NTNB 8y', 'NTNB 9y', 'NTNB 10y']]
+
+# Benchmark
+sgs = SGS()
+df_cdi = sgs.fetch({12: 'CDI'})
+df_cdi = df_cdi['CDI'] / 100
+
+# Excess Returns
+df_tri = compute_eri(df_tri, df_cdi)
 
 # Real Zero Curve
 df_raw_curve = pd.DataFrame()
@@ -55,11 +68,43 @@ df_sharpe = df_returns / df_vols
 # =======================
 dates2loop = df_tri.dropna(how='all').index
 dates2loop = dates2loop[dates2loop >= start_date]
+next_rebalance_date = dates2loop[0]
 
-df_weights = pd.DataFrame()
-for date in dates2loop:
-    vol = df_vols.loc[date]
-    mu = 0.5 * df_sharpe.loc[date] * vol
+df_weights = pd.DataFrame(columns=df_tri.columns)
+df_target_vol = pd.Series(name='Target Vol')
+for date in tqdm(dates2loop, 'Optimizations'):
 
-    # TODO Parei aqui
-    corr = df_tri.resample('Q').last().pct_change().corr()
+    if date >= next_rebalance_date:
+        vol = df_vols.loc[date]
+        mu = 0.5 * df_sharpe.loc[date] * vol
+        corr = df_tri.loc[:date].resample('M').last().pct_change().corr()
+        cov = corr2cov(corr, vol)
+
+        n_assets = len(vol)
+        w0 = np.ones(n_assets) * (1 / n_assets)
+        bounds = Bounds(np.zeros(n_assets), np.ones(n_assets))
+        cons_vol = NonlinearConstraint(fun=lambda x: np.sqrt(x @ cov @ x),
+                                       lb=min_vol, ub=np.inf)
+        cons_weight = LinearConstraint(A=np.ones(n_assets),
+                                       lb=1, ub=1)
+
+        def objfun(x):
+            v = np.sqrt(x @ cov @ x)
+            r = x @ mu
+            s = r / v
+            return -s
+
+        res = minimize(objfun, w0, bounds=bounds, constraints=(cons_vol, cons_weight))
+
+        df_target_vol.loc[date] = np.sqrt(res.x @ cov @ res.x)
+        df_weights.loc[date] = res.x
+
+        next_rebalance_date = next_rebalance_date + pd.DateOffset(months=rebalance_window)
+
+    else:
+        pass
+
+
+# TODO transformar os pesos em backtest
+
+
