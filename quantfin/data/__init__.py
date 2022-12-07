2c24a3a6b946d332b979d8b2be115935bd1d159a
gamarante <gusamarante@gmail.com>
2022-12-07 15:14:42 -0300

    Added FRED data source

--- quantfin/data/__init__.py
+++ quantfin/data/__init__.py
@@ -1,4 +1,5 @@
 from quantfin.data.utils import grab_connection, tracker_uploader, tracker_feeder, DROPBOX
 from quantfin.data.bcbsgs import SGS
+from quantfin.data.fred import FRED
 
-__all__ = ['grab_connection', 'tracker_uploader', 'tracker_feeder', 'SGS', 'DROPBOX']
+__all__ = ['grab_connection', 'tracker_uploader', 'tracker_feeder', 'SGS', 'DROPBOX', 'FRED']
