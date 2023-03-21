d8bc6f19fdeef02448595f7fb3a40d2b181c9191
gamarante <gusamarante@gmail.com>
2023-03-21 19:36:53 -0200

    Trading DI1 PCA

--- quantfin/data/strategies/di1_pca_backtest.py
+++ quantfin/data/strategies/di1_pca_backtest.py
@@ -5,31 +5,38 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from scipy.stats import percentileofscore
 from pandas.tseries.offsets import BDay, Day
+from quantfin.data import DROPBOX
+from time import time
 
 pd.set_option('display.max_columns', 25)
 pd.set_option('display.width', 250)
-
+tic = time()
 
 # User defined parameters
-window_size = 252 * 3  # TODO Hyper
-min_sample = 21  # TODO Hyper
+min_sample = 21
 start_date = '2007-01-01'
-exposition_pcadv01 = [0, 100, 0]
+exposition_pcadv01 = [0, 0, 100]
 exposition_pca_number = 3
-holding_period = 30  # TODO Hyper
 entry_bound = 5  # Must be below 50 # TODO Hyper
+writer = pd.ExcelWriter(DROPBOX.joinpath('DI1 PCA Backtest'))
 
 # get data
-df_raw = pd.read_csv(r'/Users/gustavoamarante/Dropbox/Aulas/Insper - Renda Fixa/2022/Dados DI1.csv', index_col=0)
+df_raw = pd.DataFrame()
+for year in tqdm(range(2006, 2023 + 1), 'Reading Files'):
+    aux = pd.read_csv(DROPBOX.joinpath(f'trackers/dados_di1 {year}.csv'),
+                      sep=';')
+    df_raw = pd.concat([df_raw, aux], axis=0)
+df_raw = df_raw.drop(['Unnamed: 0'], axis=1)
 df_raw['reference_date'] = pd.to_datetime(df_raw['reference_date'])
+df_raw['maturity_date'] = pd.to_datetime(df_raw['maturity_date'])
 
-# build the curve
+# # build the curve
 df_curve = df_raw.pivot('reference_date', 'du', 'rate')
 df_curve = df_curve.interpolate(axis=1, method='cubic')
 df_curve = df_curve.dropna(how='any', axis=1)
 df_curve.index = pd.to_datetime(df_curve.index)
-
-# Organize DV01
+#
+# # Organize DV01
 df_dv01 = df_raw.pivot(index='reference_date', columns='du', values='dv01')
 df_dv01.index = pd.to_datetime(df_dv01.index)
 df_dv01 = - df_dv01
@@ -37,32 +44,46 @@ df_dv01 = - df_dv01
 # ===========================
 # ===== Full sample PCA =====
 # ===========================
-pca = PCA(n_components=3)
-pca.fit(df_curve.values)
+pca_full = PCA(n_components=3)
+pca_full.fit(df_curve.values)
 
-df_var_full = pd.DataFrame(data=pca.explained_variance_ratio_)
-df_loadings_full = pd.DataFrame(data=pca.components_.T, columns=['PC 1', 'PC 2', 'PC 3'], index=df_curve.columns)
-df_mean_full = pd.DataFrame(data=pca.mean_, index=df_curve.columns, columns=['Médias'])
-df_pca_full = pd.DataFrame(data=pca.transform(df_curve.values), columns=['PC 1', 'PC 2', 'PC 3'], index=df_curve.index)
+df_var_full = pd.DataFrame(data=pca_full.explained_variance_ratio_)
+df_loadings_full = pd.DataFrame(data=pca_full.components_.T,
+                                columns=['PC 1', 'PC 2', 'PC 3'],
+                                index=df_curve.columns)
+df_mean_full = pd.DataFrame(data=pca_full.mean_, index=df_curve.columns,
+                            columns=['Médias'])
+df_pca_full = pd.DataFrame(data=pca_full.transform(df_curve.values),
+                           columns=['PC 1', 'PC 2', 'PC 3'],
+                           index=df_curve.index)
 
-signal = np.sign(df_loadings_full.iloc[0])
-df_loadings_full = df_loadings_full * signal
-df_pca_full = df_pca_full * signal
+signal_full = np.sign(df_loadings_full.iloc[0])
+df_loadings_full = df_loadings_full * signal_full
+df_pca_full = df_pca_full * signal_full
+
+df_pca_full.to_excel(writer, 'PCA Full')
+df_loadings_full.to_excel(writer, 'Loadings Full')
+df_var_full.to_excel(writer, 'Variance Full')
 
 
 # ====================
 # ===== Backtest =====
 # ====================
-# TODO rolling historical percentile, entradas e saídas e neutralidades
+# TODO montar o backtest dos 3 PCAs baseado no df_pca_roll
 # Function that selects the portfolio for a given date
-def get_portfolio(current_date, pcadv01, window):
+def get_portfolio(current_date, pcadv01):
     current_dt = pd.to_datetime(current_date)
 
     pca = PCA(n_components=3)
-    pca.fit(df_curve.loc[current_dt - BDay(window):current_dt].values)
+    data = df_curve.loc[:current_dt].values
+    pca.fit(data)
 
-    current_loadings = pd.DataFrame(data=pca.components_, index=range(1, 4), columns=df_curve.columns).T
-    current_pca = pd.DataFrame(data=pca.transform(df_curve.values), columns=range(1, 4), index=df_curve.index)
+    current_loadings = pd.DataFrame(data=pca.components_,
+                                    index=range(1, 4),
+                                    columns=df_curve.columns).T
+    current_pca = pd.DataFrame(data=pca.transform(df_curve.values),
+                               columns=range(1, 4),
+                               index=df_curve.index)
 
     signal = np.sign(current_loadings.iloc[0])
     current_loadings = current_loadings * signal
@@ -72,12 +93,12 @@ def get_portfolio(current_date, pcadv01, window):
     available_maturities = available_maturities[df_curve.columns.min() <= available_maturities]
     available_maturities = available_maturities[df_curve.columns.max() >= available_maturities]
 
-    aux_pcadv01 = current_loadings.loc[available_maturities].multiply(df_dv01.loc[current_date, available_maturities],
-                                                                      axis=0)
-    vertices_du = [aux_pcadv01.index[2], aux_pcadv01.idxmin()[3], aux_pcadv01.idxmax()[3]]
+    aux_pcadv01 = current_loadings.loc[available_maturities]
+    aux_pcadv01 = aux_pcadv01.multiply(df_dv01.loc[current_date, available_maturities], axis=0)
+    vertices_du = [aux_pcadv01.index[5], aux_pcadv01.idxmin()[3], aux_pcadv01.idxmax()[3]]
 
     if len(set(vertices_du)) != 3:
-        vertices_du = [aux_pcadv01.index[2], aux_pcadv01.idxmin()[3], aux_pcadv01.index[-1]]
+        vertices_du = [aux_pcadv01.index[3], aux_pcadv01.idxmin()[3], aux_pcadv01.index[-1]]
 
     selected_portfolio = pd.DataFrame(index=vertices_du)
     cond_date = df_raw['reference_date'] == current_date
@@ -98,33 +119,36 @@ def get_portfolio(current_date, pcadv01, window):
     return selected_portfolio, selected_signal
 
 
-# Begin
+# Begin Backtest
 df_backtest = pd.DataFrame()
 dates2loop = df_curve.index[df_curve.index >= start_date]
 df_backtest.loc[dates2loop[0], 'position'] = 0
 
-current_portfolios, current_signal = get_portfolio(dates2loop[0], exposition_pcadv01, window_size)
-df_backtest.loc[dates2loop[0], 'notional'] = (current_portfolios['quantities'] * current_portfolios['pu']).abs().sum()
+current_portfolios, current_signal = get_portfolio(current_date=dates2loop[0],
+                                                   pcadv01=exposition_pcadv01)
+df_backtest.loc[dates2loop[0], 'notional'] = (current_portfolios['quantities'].abs() * current_portfolios['pu']).sum()
 
 dates2loop = list(zip(dates2loop[1:], dates2loop[:-1]))
 for date, datem1 in tqdm(dates2loop, 'Backtesting'):
-    # Signal and portfolio will be computed everyday
-    current_portfolios, current_signal = get_portfolio(datem1, exposition_pcadv01, window_size)
-    df_backtest.loc[date, 'signal'] = current_signal.iloc[exposition_pca_number - 1]
 
-    if df_backtest.shape[0] < min_sample:  # Only compute signal until reach minimun
+    # Signal and portfolio will be computed everyday
+    current_portfolios, current_signal = get_portfolio(datem1, exposition_pcadv01)
+    df_backtest.loc[date, 'signal'] = current_signal[f'PCA {exposition_pca_number}']
+
+    if df_backtest.shape[0] < min_sample:  # assures min_sample observations before rebalances
         df_backtest.loc[date, 'position'] = 0  # Neutral to the PC
         df_backtest.loc[date, 'notional'] = df_backtest.loc[datem1, 'notional']
         continue
-
+#
     # Rolling percentile of the signal
-    df_backtest.loc[date, 'signal percentile'] = percentileofscore(a=df_backtest['signal'],  # TODO Could add a window here
-                                                                   score=df_backtest.loc[date, 'signal'],
-                                                                   kind='mean')
+    signal_percentile = percentileofscore(a=df_backtest['signal'].dropna(),
+                                          score=df_backtest.loc[date, 'signal'],
+                                          kind='weak')
+    df_backtest.loc[date, 'signal percentile'] = signal_percentile
 
     # Build Position
     if (df_backtest.loc[datem1, 'position'] == 1) or (df_backtest.loc[datem1, 'position'] == -1):
-        # If yesterday I had a position, check if the signal percentile is back at 50.
+        # If yesterday I had a position, check if the signal percentile is back at 50
         signm1 = np.sign(df_backtest.loc[datem1, 'signal percentile'] - 50)
         sign = np.sign(df_backtest.loc[date, 'signal percentile'] - 50)
         if sign == signm1:
@@ -161,6 +185,10 @@ for date, datem1 in tqdm(dates2loop, 'Backtesting'):
 
     df_backtest.loc[date, 'pnl'] = pnl_today
     df_backtest.loc[date, 'notional'] = df_backtest.loc[datem1, 'notional'] + pnl_today
-    # TODO cost
+    # TODO add cost
 
-df_backtest.to_clipboard()
+df_backtest.to_excel(writer, 'Backtest')
+
+writer.save()
+toc = time()
+print(round((toc - tic) / 60, 1), 'minutes')
--- quantfin/data/strategies/di1_rolling_pca.py
+++ quantfin/data/strategies/di1_rolling_pca.py
@@ -3,21 +3,17 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from pandas.tseries.offsets import BDay
 from quantfin.data import DROPBOX
 
 # User defined parameters
-window_size = 252 * 2
 min_sample = 21
 start_date = '2007-01-01'
-exposition_pcadv01 = [0, 0, 1000]
-exposition_pca_number = 3
-holding_period = 30
 
 # get data
 df_raw = pd.DataFrame()
-for year in tqdm(range(2006, 2023+1), 'Reading Files'):
-    aux = pd.read_csv(DROPBOX.joinpath(f'trackers/dados_di1 {year}.csv'), sep=';')
+for year in tqdm(range(2006, 2023 + 1), 'Reading Files'):
+    aux = pd.read_csv(DROPBOX.joinpath(f'trackers/dados_di1 {year}.csv'),
+                      sep=';')
     df_raw = pd.concat([df_raw, aux], axis=0)
 
 df_raw = df_raw.drop(['Unnamed: 0'], axis=1)
@@ -41,14 +37,18 @@ pca = PCA(n_components=3)
 pca.fit(df_curve.values)
 
 df_var_full = pd.DataFrame(data=pca.explained_variance_ratio_)
-df_loadings_full = pd.DataFrame(data=pca.components_.T, columns=['PC 1', 'PC 2', 'PC 3'], index=df_curve.columns)
-df_mean_full = pd.DataFrame(data=pca.mean_, index=df_curve.columns, columns=['Médias'])
-df_pca_full = pd.DataFrame(data=pca.transform(df_curve.values), columns=['PC 1', 'PC 2', 'PC 3'], index=df_curve.index)
+df_loadings_full = pd.DataFrame(data=pca.components_.T,
+                                columns=['PC 1', 'PC 2', 'PC 3'],
+                                index=df_curve.columns)
+df_mean_full = pd.DataFrame(data=pca.mean_, index=df_curve.columns,
+                            columns=['Médias'])
+df_pca_full = pd.DataFrame(data=pca.transform(df_curve.values),
+                           columns=['PC 1', 'PC 2', 'PC 3'],
+                           index=df_curve.index)
 
 signal = np.sign(df_loadings_full.iloc[0])
 df_loadings_full = df_loadings_full * signal
 df_pca_full = df_pca_full * signal
-
 
 # =========================
 # ===== Loop for PCAs =====
@@ -59,7 +59,6 @@ df_var_roll = pd.DataFrame(columns=['PC 1', 'PC 2', 'PC 3'])
 df_pca_roll = pd.DataFrame(columns=['PC 1', 'PC 2', 'PC 3'])
 
 for date in tqdm(dates2loop, 'Computing Rolling PCs'):
-    # aux_curve = df_curve.loc[date - BDay(window_size):date]  # Rolling Window
     aux_curve = df_curve.loc[:date]  # Expanding Window
 
     if aux_curve.shape[0] < min_sample:
@@ -87,10 +86,3 @@ plt.show()
 df_pca_full['PC 3'].plot()
 df_pca_roll['PC 3'].plot()
 plt.show()
-
-
-# ====================
-# ===== Backtest =====
-# ====================
-# TODO montar o backtest dos 3 PCAs baseado no df_pca_roll
-# TODO rolling historical percentile, entradas e saídas e neutralidades
